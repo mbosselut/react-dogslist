ddbfe947500759bd2560df3338162b0cca1b2253
mbosselut <duchmol7@hotmail.com>
2019-10-24 11:19:04 +0200

    Finished DogsList app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import DogsListContainer from "./components/DogsListContainer";
+import DogBreedImagesContainer from "./components/DogBreedImagesContainer";
 import "./App.css";
 
 function App() {
     return (
         <div className="App">
             <main>
-                <DogsListContainer />
+                <Route exact path="/" component={DogsListContainer} />{" "}
+                <Route
+                    path="/dog-breeds/:breed"
+                    component={DogBreedImagesContainer}
+                />
             </main>
         </div>
     );
